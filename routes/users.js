12306decd950830047279c88e59fa297c862b971
DocOch <Ochmanowicz@gmail.com>
2018-01-12 15:56:04 -0500

    all 4 basic api routes to db working

--- routes/users.js
+++ routes/users.js
@@ -14,17 +14,54 @@ router.get('/users', function(req, res, next){
 	//async deathzone, anything palced here is undefined
 })
 
-router.post('/users', function(req, res, next){
-	console.log("POST REQUEST BODY IS ", req.body)
-	// db.save({}, function(err, users){
-	// 	if(err){
-	// 		console.log('THERE WAS AN ERROR ', err)
-	// 		return;
-	// 	}
-	// 	res.json(users);
-	// })
+router.get('/users/:id', function(req, res, next){
+	db.findOne({_id: req.params.id}, function(err, users){
+		if(err){
+			console.log('THERE WAS AN ERROR ', err)
+			return;
+		}
+		res.json(users);
+	})
 	//async deathzone, anything palced here is undefined
-	res.send('end')
+})
+
+router.post('/users', function(req, res, next){
+	var user = new db(req.body);
+    db.findOne(req.body, function (err, success) {
+        if (err) {
+            console.log(err);
+            res.send(err);
+        }
+        else {
+            console.log(success);
+            if (success == null) {
+                user.save(function (err, success) {
+                    if (err) {
+                        console.log(err);
+                        res.send(err);
+                    }
+                    else {
+                        console.log("inserted");
+                        console.log(success);
+                        res.send("success");
+                    }
+                });
+            } else {
+                res.send("User already present");
+            }
+        }
+    })
+	//async deathzone, anything palced here is undefined
+})
+
+router.delete('/users/:id', function(req, res, next){
+	db.remove({_id: req.params.id}, function(err, data){
+		if(err){
+			console.log('THERE WAS AN ERROR')
+			return;
+		}
+		res.json('1')
+	})
 })
 /*
 localhost:5000/users (GET) => fetches the entire database
